7c0f24bdb66edd95b87767e9f70b44d1df5b5bdb
AnkushS27 <ankush27singh11@gmail.com>
2024-02-09 19:14:30 +0530

    loginSignup page added

---
+++ src/app/cart/page.tsx
@@ -0,0 +1,11 @@
+import styles from './page.module.css'
+
+export default function Cart() {
+    return (
+        <div className={styles.mainWrapper}>
+            <div className={styles.cartContainer}>
+                this is cart
+            </div>
+        </div>
+    )
+}
