ebd8acef0d52ba04ae3379dc3ac6b5c26075d356
Anirudh3167 <77240291+Anirudh3167@users.noreply.github.com>
2024-02-12 14:07:18 +0530

    login signup pages created

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,42 @@
+"use client";
+import { useState } from "react";
+import styles from "./page.module.css";
+import Link from "next/link";
+
+export default function LoginSignup() {
+  const [uname, setUname] = useState('');
+  const [email,setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [showPass,setShowPass] = useState(false);
+  const handleLoginClick = () => {
+    console.log('Login Request');
+  }
+  return (
+    <div className={styles.mainWrapper}>
+      <div className={styles.loginSignup}>
+        <div className={styles.loginSignupContainer}>
+              <h1>Sign Up</h1>
+              <div className={styles.loginSignupFields}>
+                {/* Add signup form fields */}
+                <input type="email" placeholder="Email Address" />
+                <div className={styles.passwordCotainer}>
+                  <input type={showPass ? "text" : "password"} placeholder="Password" style={{paddingRight:'70px'}} />
+                  <div className={styles.passwordShowBtn} onClick={() => {setShowPass(!showPass)}}>{showPass ? "hide" : "show"}</div>
+                </div>
+              </div>
+              <button onClick={handleLoginClick}>Continue</button>
+              <p className={styles.loginSignupLogin}>
+                Don't have an account?{" "}
+                <Link href='/signup'>Sign Up</Link>
+              </p>
+              <div className={styles.loginSignupAgree}>
+                <input type="checkbox" name="" id="" style={{width:'30px',height:'30px',background:'white',color:'black',borderRadius:'10px'}} />
+                <p>
+                  By continuing, I agree to the <Link href='#'>terms</Link> of use & <Link href='#'>privacy ploicy</Link>
+                </p>
+              </div>
+        </div>
+      </div>
+    </div>
+  );
+}
