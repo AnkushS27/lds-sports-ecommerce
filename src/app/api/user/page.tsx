9ce3382a22c6dd394afa84fbe63634cb22ecae0b
AnkushS27 <ankush27singh11@gmail.com>
2024-02-18 16:15:52 +0530

    minor change

--- src/app/api/user/page.tsx
+++
@@ -1,9 +0,0 @@
-// import { getCsrfToken } from "next-auth/react"
-
-// export default async function Testing() {
-//     const csrfToken = getCsrfToken;
-//     console.log(csrfToken);
-//     return(
-//         <h1>Check console</h1>
-//     )
-// }
