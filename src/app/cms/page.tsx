ceffc3275df757e2e329a127f875fd0ab89f6ab3
AnkushS27 <ankush27singh11@gmail.com>
2024-02-21 23:27:29 +0530

    cms dashboard basic ui added

--- src/app/cms/page.tsx
+++ src/app/cms/page.tsx
@@ -1,23 +1,94 @@
-import VerticalNavBar from '@/Components/CMS/VerticalNavbar/page';
-import styles from './page.module.css';
+import VerticalNavBar from "@/Components/CMS/VerticalNavbar/page";
+import styles from "./page.module.css";
 
-import { loggedIn } from '../api/user/loggedIn';
+import { loggedIn } from "../api/user/loggedIn";
+
+import { FaSackDollar } from "react-icons/fa6";
+import { AiFillCreditCard } from "react-icons/ai";
+import { MdInventory } from "react-icons/md";
 
 export default function Home() {
-    const isloggedIn = loggedIn({});
+  const isloggedIn = loggedIn({});
   return (
     <div className={styles.mainWrapper}>
-        <VerticalNavBar params={{name:'ABC',loggedIn: isloggedIn}}/>
-        <div className={styles.cmsContent}>
-            <h2>Dashboard</h2>
-            <div className={styles.mainContent}>
-                Main content of CMS will be displayed here.
-            </div>
-            <div className="recentSectionContainer">
-              <div className="sectionHead"> Recent Activity </div>
-              <div className="sectionContainer"> No recent activity here. </div>
-            </div>
+      <VerticalNavBar params={{ name: "ABC", loggedIn: isloggedIn }} />
+      <div className={styles.cmsContent}>
+        <h2>Dashboard</h2>
+        <div className={styles.topContainer}>
+          <div className={styles.card}>
+            <h3>
+              Total Revenue
+              <FaSackDollar />
+            </h3>
+            <p>Rs. 1,512.00</p>
+          </div>
+          <div className={styles.card}>
+            <h3>
+              Total Orders
+              <AiFillCreditCard />
+            </h3>
+            <p>+62</p>
+          </div>
+          <div className={styles.card}>
+            <h3>
+              Products in Stock
+              <MdInventory />
+            </h3>
+            <p>2</p>
+          </div>
         </div>
+        <div className={styles.graph}>
+          <p>Graph will be displayed here...</p>
+        </div>
+        <div className={styles.recentSectionContainer}>
+          <h3>Recent Orders</h3>
+          <div className={styles.table}>
+            <table>
+              <thead>
+                <tr>
+                  <th>Order</th>
+                  <th>Date</th>
+                  <th>Customer</th>
+                  <th>Location</th>
+                  <th>Fulfillment</th>
+                  <th>Payment</th>
+                  <th>Total</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>#12345</td>
+                  <td>09/20/22</td>
+                  <td>John Smith</td>
+                  <td>San Francisco, CA</td>
+                  <td>Not fulfilled</td>
+                  <td>Credit card</td>
+                  <td>Rs. 120.00</td>
+                </tr>
+                <tr>
+                  <td>#12345</td>
+                  <td>09/20/22</td>
+                  <td>John Smith</td>
+                  <td>San Francisco, CA</td>
+                  <td>Not fulfilled</td>
+                  <td>Credit card</td>
+                  <td>Rs. 120.00</td>
+                </tr>
+                <tr>
+                  <td>#12345</td>
+                  <td>09/20/22</td>
+                  <td>John Smith</td>
+                  <td>San Francisco, CA</td>
+                  <td>Not fulfilled</td>
+                  <td>Credit card</td>
+                  <td>Rs. 120.00</td>
+                </tr>
+                {/* Add more rows for other recent orders */}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
