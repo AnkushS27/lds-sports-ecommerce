1c64d9bc71e4b8026bd99309eacef93b87e8458c
AnkushS27 <ankush27singh11@gmail.com>
2024-02-25 02:16:28 +0530

    minor product page ui change

--- src/app/product/[Id]/page.tsx
+++ src/app/product/[Id]/page.tsx
@@ -11,6 +11,9 @@ import { useState } from "react";
 
 // import { getData } from "@/db/testing";
 
+//icons import
+import { BsCaretUpFill, BsCaretDownFill } from "react-icons/bs";
+
 let product = {
   pid: "001",
   name: "cricket Bat",
@@ -18,20 +21,19 @@ let product = {
   price: "₹1000",
   tags: ["bat", "cricket bat", "cricket", "kokabura"],
   desc: "Product 1 is one the most selled products from the company 1. Limited products so buy soon.",
-  Variations: {name:'Sizes', variations: [
-    { size: 6, stock: 40, price: "₹120" },
-    { size: 7, stock: 40, price: "₹10" },
-    { size: 8, stock: 40, price: "₹130" },
-    { size: 9, stock: 40, price: "₹180" },
-    { size: 10, stock: 40, price: "₹210" },
-  ]},
+  Variations: {
+    name: "Sizes",
+    variations: [
+      { size: 6, stock: 40, price: "₹120" },
+      { size: 7, stock: 40, price: "₹10" },
+      { size: 8, stock: 40, price: "₹130" },
+      { size: 9, stock: 40, price: "₹180" },
+      { size: 10, stock: 40, price: "₹210" },
+    ],
+  },
 };
 
-export default function ProductDetails({
-  params,
-}: {
-  params: { Id: string };
-}) {
+export default function ProductDetails({ params }: { params: { Id: string } }) {
   const isloggedIn = loggedIn({});
   const [variationIdx, setVariationIdx] = useState(0);
   // const data = await getData();
@@ -45,22 +47,30 @@ export default function ProductDetails({
           <div className={style1.productContainer}>
             <div className={style1.productDetailsContainer}>
               <div className={style1.LeftSection}>
-                <div className={style1.ImgsContainer}>
-                  <div className={style1.ImgsHolder}>
-                    <div className={style1.ImgSmallBox}></div>
-                    <div
-                      className={`${style1.ImgSmallBox} ${style1.activeImgBox}`}
-                    ></div>
-                    <div className={style1.ImgSmallBox}></div>
-                    <div className={style1.ImgSmallBox}></div>
+                <div className={style1.imageContainer}>
+                  <div className={style1.ImgsContainer}>
+                    <div className={style1.ImgsHolder}>
+                      <div className={style1.ImgSmallBox}></div>
+                      <div
+                        className={`${style1.ImgSmallBox} ${style1.activeImgBox}`}
+                      ></div>
+                      <div className={style1.ImgSmallBox}></div>
+                      <div className={style1.ImgSmallBox}></div>
+                    </div>
+                    <div className={style1.ImgsController}>
+                      <div className={style1.ImgsControllBtn}><BsCaretUpFill /></div>
+                      <div className={style1.ImgsControllBtn}><BsCaretDownFill/></div>
+                    </div>
                   </div>
-                  <div className={style1.ImgsController}>
-                    <div className={style1.ImgsControllBtn}>^</div>
-                    see more
-                    <div className={style1.ImgsControllBtn}>v</div>
+                  <div className={style1.ImgContainer}> Active Image here </div>
+                </div>
+                <div className={style1.productButtons}>
+                  <div className={style1.productCartBtn}>+ Add to Cart</div>
+                  <div className={style1.productBuyBtn}>
+                    {" "}
+                    Add to Favourites{" "}
                   </div>
                 </div>
-                <div className={style1.ImgContainer}> Active Image here </div>
               </div>
               <div className={style1.RightSection}>
                 <div className={style1.productTitleHead}>{product.name}</div>
@@ -76,32 +86,41 @@ export default function ProductDetails({
                 <div className={style1.productCompany}>{product.company}</div>
                 <div className={style1.productTitleDesc}>{product.desc}</div>
                 <div className={style1.productDiversityContainer}>
-                  <div className={style1.productVaritionsName}>{product.Variations.name}</div>
+                  <div className={style1.productVaritionsName}>
+                    {product.Variations.name}
+                  </div>
                   <div className={style1.productVariationsHolder}>
-                  {product.Variations.variations.map((diversity, index) => {
-                    return (
-                        <div className={style1.productDiversityItem} style={index==variationIdx ? {backgroundColor:'white',color:'black'}:{}}
-                        key={index} onClick={() => {setVariationIdx(index)}}>{diversity.size}</div>
-                    );
-                  })}
+                    {product.Variations.variations.map((diversity, index) => {
+                      return (
+                        <div
+                          className={style1.productDiversityItem}
+                          style={
+                            index == variationIdx
+                              ? { backgroundColor: "white", color: "black" }
+                              : {}
+                          }
+                          key={index}
+                          onClick={() => {
+                            setVariationIdx(index);
+                          }}
+                        >
+                          {diversity.size}
+                        </div>
+                      );
+                    })}
                   </div>
                 </div>
                 <div className="productPriceContainer">
                   <div className="productPriceHolder">
                     {/* Previous Product Price */}
-                    <div className={style1.productPrice}>{product.Variations.variations[variationIdx].price}</div>
+                    <div className={style1.productPrice}>
+                      {product.Variations.variations[variationIdx].price}
+                    </div>
                     {/* Price After the discount */}
                   </div>
                   <div className="productDiscountOptions">
                     {/* <div className="productDiscountHead">Discount</div> */}
                     {/* Discount percentage */}
-                  </div>
-                </div>
-                <div className={style1.productButtons}>
-                  <div className={style1.productCartBtn}>+ Add to Cart</div>
-                  <div className={style1.productBuyBtn}>
-                    {" "}
-                    Add to Favourites{" "}
                   </div>
                 </div>
               </div>
