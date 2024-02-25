96734b5588fb7afc4cbb6a4465e10cc02dc4eae5
AnkushS27 <ankush27singh11@gmail.com>
2024-02-25 22:56:31 +0530

    cms users page added

---
+++ src/app/cms/customers/page.tsx
@@ -0,0 +1,54 @@
+// pages/users.tsx
+
+import styles from './page.module.css';
+import VerticalNavBar from "@/Components/CMS/VerticalNavbar/page"
+
+const Users: React.FC = () => {
+  // Fetch user data from your API or mock data
+  const users = [
+    { id: 673521, name: 'John Doe', email: 'john@example.com' },
+    { id: 262563, name: 'Jane Doe', email: 'jane@example.com' },
+    // Add more user data as needed
+  ];
+
+  return (
+    <div className={styles.mainWrapper}>
+            <VerticalNavBar
+              params={{ name: "ABC", loggedIn: true }}
+            />
+            <div className={styles.VerticalMainContainer}>
+      <h1>Manage Users</h1>
+            <div className={styles.usersContainer}>
+      <table className={styles.userTable}>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr key={user.id}>
+              <td>{user.id}</td>
+              <td>{user.name}</td>
+              <td>{user.email}</td>
+              <td>
+                {/* Add buttons or links for managing user accounts */}
+                <button>Edit</button>
+                <button>Orders</button>
+                <button>Favourites</button>
+                <button>Delete User</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+            </div>
+    </div>
+  );
+};
+
+export default Users;
--- src/app/cms/page.tsx
+++ src/app/cms/page.tsx
@@ -11,81 +11,85 @@ export default function Home() {
   const isloggedIn = loggedIn({});
   return (
     <div className={styles.mainWrapper}>
-      <VerticalNavBar params={{ name: "ABC", loggedIn: isloggedIn }} />
-      <div className={styles.cmsContent}>
-        <h2>Dashboard</h2>
-        <div className={styles.topContainer}>
-          <div className={styles.card}>
-            <h3>
-              Total Revenue
-              <FaSackDollar />
-            </h3>
-            <p>Rs. 1,512.00</p>
-          </div>
-          <div className={styles.card}>
-            <h3>
-              Total Orders
-              <AiFillCreditCard />
-            </h3>
-            <p>+62</p>
-          </div>
-          <div className={styles.card}>
-            <h3>
-              Products in Stock
-              <MdInventory />
-            </h3>
-            <p>2</p>
-          </div>
-        </div>
-        <div className={styles.graph}>
-          <p>Graph will be displayed here...</p>
-        </div>
-        <div className={styles.recentSectionContainer}>
-          <h3>Recent Orders</h3>
-          <div className={styles.table}>
-            <table>
-              <thead>
-                <tr>
-                  <th>Order</th>
-                  <th>Date</th>
-                  <th>Customer</th>
-                  <th>Location</th>
-                  <th>Fulfillment</th>
-                  <th>Payment</th>
-                  <th>Total</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>#12345</td>
-                  <td>09/20/22</td>
-                  <td>John Smith</td>
-                  <td>San Francisco, CA</td>
-                  <td>Not fulfilled</td>
-                  <td>Credit card</td>
-                  <td>Rs. 120.00</td>
-                </tr>
-                <tr>
-                  <td>#12345</td>
-                  <td>09/20/22</td>
-                  <td>John Smith</td>
-                  <td>San Francisco, CA</td>
-                  <td>Not fulfilled</td>
-                  <td>Credit card</td>
-                  <td>Rs. 120.00</td>
-                </tr>
-                <tr>
-                  <td>#12345</td>
-                  <td>09/20/22</td>
-                  <td>John Smith</td>
-                  <td>San Francisco, CA</td>
-                  <td>Not fulfilled</td>
-                  <td>Credit card</td>
-                  <td>Rs. 120.00</td>
-                </tr>
-                {/* Add more rows for other recent orders */}
-              </tbody>
-            </table>
+      <VerticalNavBar params={{ name: "ABC", loggedIn: true }} />
+      <div className={styles.VerticalMainContainer}>
+        <div className={styles.homeContainer}>
+          <div className={styles.cmsContent}>
+            <h2>Dashboard</h2>
+            <div className={styles.topContainer}>
+              <div className={styles.card}>
+                <h3>
+                  Total Revenue
+                  <FaSackDollar />
+                </h3>
+                <p>Rs. 1,512.00</p>
+              </div>
+              <div className={styles.card}>
+                <h3>
+                  Total Orders
+                  <AiFillCreditCard />
+                </h3>
+                <p>+62</p>
+              </div>
+              <div className={styles.card}>
+                <h3>
+                  Products in Stock
+                  <MdInventory />
+                </h3>
+                <p>2</p>
+              </div>
+            </div>
+            <div className={styles.graph}>
+              <p>Graph will be displayed here...</p>
+            </div>
+            <div className={styles.recentSectionContainer}>
+              <h3>Recent Orders</h3>
+              <div className={styles.table}>
+                <table>
+                  <thead>
+                    <tr>
+                      <th>Order</th>
+                      <th>Date</th>
+                      <th>Customer</th>
+                      <th>Location</th>
+                      <th>Fulfillment</th>
+                      <th>Payment</th>
+                      <th>Total</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>#12345</td>
+                      <td>09/20/22</td>
+                      <td>John Smith</td>
+                      <td>San Francisco, CA</td>
+                      <td>Not fulfilled</td>
+                      <td>Credit card</td>
+                      <td>Rs. 120.00</td>
+                    </tr>
+                    <tr>
+                      <td>#12345</td>
+                      <td>09/20/22</td>
+                      <td>John Smith</td>
+                      <td>San Francisco, CA</td>
+                      <td>Not fulfilled</td>
+                      <td>Credit card</td>
+                      <td>Rs. 120.00</td>
+                    </tr>
+                    <tr>
+                      <td>#12345</td>
+                      <td>09/20/22</td>
+                      <td>John Smith</td>
+                      <td>San Francisco, CA</td>
+                      <td>Not fulfilled</td>
+                      <td>Credit card</td>
+                      <td>Rs. 120.00</td>
+                    </tr>
+                    {/* Add more rows for other recent orders */}
+                  </tbody>
+                </table>
+              </div>
+            </div>
           </div>
         </div>
       </div>
