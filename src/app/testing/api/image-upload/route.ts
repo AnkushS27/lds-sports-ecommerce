9269e3cac59f6c16356161a1eb3b38477d9306f5
Anirudh3167 <77240291+Anirudh3167@users.noreply.github.com>
2024-03-02 00:14:48 +0530

    file upload is working now

--- src/app/testing/api/image-upload/route.ts
+++ src/app/testing/api/image-upload/route.ts
@@ -1,44 +1,29 @@
-// // route.ts
-
-// import { NextRequest, NextResponse } from "next/server";
-
-// export async function POST(req: NextRequest) {
-
-//     const files = await req?.formData();
-
-//     console.log(files);
-
-//     return NextResponse.json({message:'success'});
-// }
-
-// pages/api/upload.ts
-
+import { writeFile } from 'fs/promises';
 import { NextRequest, NextResponse } from 'next/server';
-import { ConnectDatabase } from '@/db/db_connections/Connector';
-import ImageModel from '@/db/models/imageModel';
 
 export async function POST(req: NextRequest) {
-    // Connect to MongoDB
-    await ConnectDatabase();
-
     // Access the uploaded files
     const formData = await req?.formData();
-    const files = formData ? Array.from(formData.values()) : [];
+    const files = formData ? Array.from(formData.entries()) : [];
 
     if (!files || files.length === 0) {
-        return NextResponse.json({message:'No files uploaded'},{status:404});
+        return NextResponse.json({ message: 'No files uploaded' }, { status: 404 });
     }
 
     try {
-        // Save each uploaded image to MongoDB
-        for (const file of files) {
-            const image = new ImageModel();
-            image.data = file;
-            await image.save();
-        }
-        return NextResponse.json({ message: 'Images uploaded successfully' },{ status:200});
+        // Save each uploaded image in File System
+        files.forEach(async ([name, file]) => {
+            if (file instanceof File) {
+                const byteData = await file.arrayBuffer();
+                const buffer = Buffer.from(byteData);
+                const path = `./public/${file.name}`;
+                await writeFile(path,buffer);
+            }
+        });
+
+        return NextResponse.json({ message: 'Images uploaded successfully' }, { status: 200 });
     } catch (error) {
         console.error('Error uploading images:', error);
-        return NextResponse.json({ error: 'Internal server error' },{ status: 500});
+        return NextResponse.json({ error: 'Internal server error' }, { status: 500 });
     }
 }
