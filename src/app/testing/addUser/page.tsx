32e663bd32f40e30cc24abc7a047b398d1932725
Ankush Singh <112239807+AnkushS27@users.noreply.github.com>
2024-03-03 10:41:47 +0530

    Error removed

--- src/app/testing/addUser/page.tsx
+++ src/app/testing/addUser/page.tsx
@@ -1,7 +1,7 @@
 import { addUser } from "@/db/db_connections/user";
 
 export default function UserTesting() {
-    const res = addUser('abc@gmail.com','1234')
+    const res = addUser('abc','abc@gmail.com','1234')
     return(
         <>{JSON.stringify(res)}</>
     )
